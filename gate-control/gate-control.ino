f98814eee9873af9110aed101a42cc8ffd9beca6
Flavio Tavares <flaviotvrs@gmail.com>
2020-10-20 19:24:48 -0300

    new way of receiving information

--- gate-control/gate-control.ino
+++ gate-control/gate-control.ino
@@ -1,31 +1,32 @@
-#include <RCSwitch.h>
+#include <VirtualWire.h>
 
-RCSwitch myswitch = RCSwitch();
+#define pinRF  12
+
+
+struct Token {
+  int code;
+};
+
+Token token;
+uint8_t buf[sizeof(token)];
+uint8_t buflen = sizeof(token);
 
 void setup() {
+  vw_set_rx_pin(pinRF);
+  vw_setup(2000);
+  vw_rx_start();
   Serial.begin(9600);
-  myswitch.enableReceive(0);
-  Serial.println("Receiver configured!");
 }
 
 void loop() {
 
-  if (myswitch.available()) {
+  if ( vw_have_message() ) {
+    vw_get_message(buf, &buflen);
+    memcpy(&token,&buf,buflen);
 
-    int value = myswitch.getReceivedValue();
+    
+    Serial.print("Recebido: ");
+    Serial.println(token.code);
 
-    if (value == 0) {
-      Serial.println("Unknown encoding!");
-    } else {
-      Serial.print("Received: ");
-      Serial.println(value);
-    }
-
-    mySwitch.resetAvailable();
-
-  } else {
-    Serial.println("Not available!");
-  }
-  
-
+  }  
 }
